2f866604c43706e9a0e27c8e5600ab5bb4b30dbc
shirleypaz <shirley.pazz@gmail.com>
2018-04-10 22:41:08 -0300

    Lista de tweets criada e página de login

--- twitelum-front/src/App.js
+++ twitelum-front/src/App.js
@@ -10,59 +10,84 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            novoTweet: ''
+            novoTweet: '',
+            tweets: [],
         }
+        this.adicionaTweet = this.adicionaTweet.bind(this)
     }
 
-    //140 caracteres por tweet
-    //botao disabled
-    //adicionar contador dinâmico de caracteres
-    //adicionar classe nos caracteres
+    adicionaTweet(event) {
+        event.preventDefault()
+        const novoTweet = this.state.novoTweet
+        
+        this.setState(
+            {
+                tweets: [novoTweet, ...this.state.tweets],
+                novoTweet: ''
+            }
+        )
+
+    }
+
+    //evita que dê refresh no submit - ok
+    //resetar o valor do novo tweet
+    // pegar o value do input - ok
+    // criar uma lista de inputs - ok
+    //jogar o valor do tweet na lista -ok
+    //incluir no corpo do widget com map - ok
+    //incluir props no componente
 
 
-  render() {
-    return (
-      <Fragment>
-        <Cabecalho>
-            <NavMenu usuario="@omariosouto" />
-        </Cabecalho>
-        <div className="container">
-            <Dashboard>
-                <Widget>
-                    <form className="novoTweet">
-                        <div className="novoTweet__editorArea">
-                            <span className={`novoTweet__status ${this.state.novoTweet.length > 140 
-                            ? 'novoTweet__status--invalido' : ''}
+    render() {
+        return (
+            <Fragment>
+                <Cabecalho>
+                    <NavMenu usuario="@omariosouto" />
+                </Cabecalho>
+                <div className="container">
+                    <Dashboard>
+                        <Widget>
+                            <form className="novoTweet" onSubmit={this.adicionaTweet}>
+                                <div className="novoTweet__editorArea">
+                                    <span className={`novoTweet__status ${this.state.novoTweet.length > 140
+                                        ? 'novoTweet__status--invalido' : ''}
                             `}>
-                            {this.state.novoTweet.length}/140</span>
-                            <textarea 
-                            onChange={(event) => {this.setState({ novoTweet : event.target.value})}}
-                            className="novoTweet__editor" 
-                            placeholder="O que está acontecendo?"></textarea>
-                        </div>
-                        <button 
-                        type="submit" 
-                        className="novoTweet__envia"
-                        disabled={this.state.novoTweet.length > 140 ? true : false}>
-                        Tweetar
+                                        {this.state.novoTweet.length}/140</span>
+                                    <textarea
+                                        value={this.state.novoTweet}
+                                        onChange={(event) => { this.setState({ novoTweet: event.target.value }) }}
+                                        className="novoTweet__editor"
+                                        placeholder="O que está acontecendo?"></textarea>
+                                </div>
+                                <button
+                                    type="submit"
+                                    className="novoTweet__envia"
+                                    disabled={this.state.novoTweet.length > 140 ? true : false}>
+                                    Tweetar
                         </button>
-                    </form>
-                </Widget>
-                <Widget>
-                    <TrendsArea />
-                </Widget>
-            </Dashboard>
-            <Dashboard posicao="centro">
-                <Widget>
-                    <div className="tweetsArea">
-                        <Tweet />
-                    </div>
-                </Widget>
-            </Dashboard>
-        </div>
-      </Fragment>
-    );
-  }
+                            </form>
+                        </Widget>
+                        <Widget>
+                            <TrendsArea />
+                        </Widget>
+                    </Dashboard>
+                    <Dashboard posicao="centro">
+                        <Widget>
+                            <div className="tweetsArea">
+                                {this.state.tweets.map(
+                                    (tweet, index) => {
+                                       return <Tweet
+                                            key={tweet}
+                                            texto={tweet} />
+                                    })
+                                }
+                            </div>
+                        </Widget>
+                    </Dashboard>
+                </div>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
